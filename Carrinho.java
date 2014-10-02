e67a7187cb6728c1c0c66e4f57247ebc919d22a2
gutti95 <gu.tiagosilva@gmail.com>
2014-10-02 19:09:03 -0300

    Create Carrinho.java

---
+++ Carrinho.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrinho {
+
+    private List<Produto> produtos;
+
+    public Carrinho() {
+        produtos = new ArrayList<>();
+    }
+
+    public void add(Produto produto) {
+        produtos.add(produto);
+    }
+
+    public Produto menorProduto() throws CarrinhoVazioExpected {
+        if (produtos.isEmpty()) {
+            throw new CarrinhoVazioExpected();
+        }
+        Produto menor = produtos.get(0);
+        for (Produto produto : produtos) {
+            if (produto.getPreco() < menor.getPreco()) {
+                menor = produto;
+            }
+        }
+        return menor;
+    }
+}
